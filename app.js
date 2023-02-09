a0ce2c350e85422497787aaa3ba075c8f1a98618
Justin Garber <JustinGarber@outlook.com>
2023-02-08 19:49:47 -0800

    def js file update

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
-const h1 = document.querySelector("h1");
+const gameContainer = document.getElementById("game");
 
-hello
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+// here is a helper function to shuffle an array
+// function sattoloVariationShuffleColors(COLORS) {
+//   for (let i = COLORS.length -1; i>0; i--){
+//     let j = Math.floor(Math.random() * (i+1));
+//     [COLORS[i], COLORS[j]] = [COLORS[j], COLORS[i]];
+// }
+// return COLORS}// it returns the same array with values shuffled
+
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+// TODO: Implement this function!
+function handleCardClick(event) { 
+  // you can use event.target to see which element was clicked
+  console.log("you just clicked", event.target);
+}
+
+// when the DOM loads
+createDivsForColors(shuffledColors);
+
+/* */
